76bfa2375ac6c74df3a860c8184a96507d0d4d2b
Kojihyung <kokoleerh@naver.com>
2021-06-02 00:53:50 +0900

    노가다 버전

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -4,7 +4,7 @@
  */
 
 #include "backtrack.h"
-#include "stdio.h"
+#include <stdio.h>
 #include <vector>
 #include <utility>
 #include <algorithm>
@@ -35,9 +35,7 @@ struct Node {
 Backtrack::Backtrack() {}
 Backtrack::~Backtrack() {}
 
-vector<Node> findDAG(const Graph &query,
-                                const CandidateSet &cs){
-
+vector<Node> findDAG(const Graph &query, const CandidateSet &cs){
   int num_vertices = query.GetNumVertices();
 
   vector<Node> NodeVec;
@@ -71,7 +69,7 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
   vector<Node> DAGvec = findDAG(query, cs);
   int cs_size;
   int tempsize;
-  int tempsize2;
+  int deletedcnt = 0;
 
   vector<int> candidateArr[num_vertices];
 
@@ -82,14 +80,12 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
     }
   }
 
-  // 딱 한 번만 필터링 해봄.. 함수화 시킬 예정
-
+  /*
   for (int i = 0; i < num_vertices; i++)
   {
     cs_size = candidateArr[i].size();
     tempsize = DAGvec[i].next.size();
     
-
     for (int j = 0; j < cs_size; j++)
     {
       int success = 0;
@@ -106,12 +102,13 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
         {
           int next = candidateArr[DAGvec[i].next[k]][l];
           if (data.IsNeighbor(prev, next)){
-            printf("%d %d\n", prev, next);
+            //printf("%d %d\n", prev, next);
             success++;
             break;
           }
         }
       }
+
       if(success < tempsize){
         vector<int> v = candidateArr[i];
         printf("deleted %d\n", prev);
@@ -119,30 +116,225 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
       }
     }
   }
+  */
+
   
-  /*
-  printf("\nNEXT\n");
-  for (int i = 0; i < num_vertices; i++){
-    printf("%d: ", i);
-    for (int j = 0; j < DAGvec[i].next.size(); j++)
-    {
-      printf("%d ", DAGvec[i].next[j]);
-    }
-    printf("\n");
+  // t edge
+  int cnt = 0;
+  int take[num_vertices];
+  int maxindex[num_vertices];
+  int index[num_vertices] = {
+      0,
+  };
+
+  bool failure = false;
+
+  for(int i = 0; i < num_vertices; i++){
+    maxindex[i] = candidateArr[i].size()-1;
   }
-  */
+
+  bool end = true;
+  bool add = false;
+  int point = 1;
+
+  while (true)
+  {
+    failure = false;
+    end = false;
+    add = false;
+  
+    for (int i = 0; i < num_vertices; i++){
+      if(index[i] != maxindex[i]){
+        break;
+      }
+      if(i == num_vertices-1){
+        end = true;
+      }
+    }
+
+    if(end == true)
+      break;
+      
+    for (int i = 0; i < point; i++)
+    {
+      if (index[i] < maxindex[i])
+      {
+        index[i]++;
+        add = true;
+        break;
+      }
+      else if (index[i] == maxindex[i])
+      {
+        index[i] = 0;
+      }
+    }
+
+    if(add == false){
+      while(point < num_vertices){
+        if(index[point] < maxindex[point]){
+          index[point]++;
+          point++;
+          break;
+        }
+        else{
+          point++;
+        }
+      }
+      if(point == num_vertices)
+        break;
+    }
+  /*
+    for (int i = 0; i < num_vertices; i++){
+        printf("%d ", index[i]);
+      }
+    printf("\n");
+*/
+    
+    int temp;
+    for (int i = 0; i < num_vertices; i++)
+    {
+      tempsize = DAGvec[i].next.size();
+      for (int j = 0; j < tempsize; j++){
+        temp = DAGvec[i].next[j];
+        if (!data.IsNeighbor(candidateArr[i][index[i]], candidateArr[temp][index[temp]]))
+        {
+          failure = true;
+          //printf("%d %d\n", candidateArr[i][index[i]], candidateArr[temp][index[temp]]);
+          break;
+        }
+      }
+      if(failure == true){
+        break;
+      }
+    }
+
+    if(failure == false){
+      for (int i = 0; i < num_vertices; i++){
+        printf("%d ", candidateArr[i][index[i]]);
+      }
+      printf("\n");
+    }
+  }
 
   /*
 
-  printf("\nPREV\n");
-  for (int i = 0; i < num_vertices; i++){
-    printf("%d: ", i);
-    for (int j = 0; j < DAGvec[i].prev.size(); j++)
-    {
-      printf("%d ", DAGvec[i].prev[j]);
-    }
-    printf("\n");
-  }
-  */
+  while(cnt < 10){
+    bool visited[num_vertices];
+    int take[num_vertices];
+    int index[num_vertices] = {0, };
 
+    // 가져간 index들은 -1로, visited는 false로 초기화
+    for (int i = 0; i < num_vertices; i++){
+      visited[i] = false;
+      take[i] = -1;
+    }
+
+    int target = 0;
+    bool done = true;
+    bool change = false;
+    bool choose = false;
+    
+    printf("start matching\n");
+
+    while (true)
+    {
+      done = true;
+      choose = false;
+      // target에 대한 처리와 take 변경
+      printf("target: %d\n", target);
+      int length = DAGvec[target].prev.size();
+      int length_ = candidateArr[target].size();
+
+      printf("length: %d, length_: %d\n", length, length_);
+      int number;
+      if (length == 0)
+        choose = true; // 흠..
+      
+      for (int j = 0; j < length_; j++)
+      {
+        number = candidateArr[target][j];
+        for (int i = 0; i < length; i++)
+        {
+          if(take[DAGvec[target].prev[i]] == -1)
+            break;
+          if (!data.IsNeighbor(take[DAGvec[target].prev[i]], number))
+          {
+            break;
+          }
+          if(i == length - 1){
+            choose = true;
+          }
+        }
+        if(choose == true)
+          break;
+      }
+
+      visited[target] = true;
+      take[target] = number;
+      change = false;
+      for (int i = 0; i < num_vertices; i++)
+      {
+        if(visited[i] == false){
+          done = false;
+          break;
+        }
+      }
+
+      if(done == true) break;
+      while(change == false)
+      {
+        cs_size = candidateArr[target].size();
+        tempsize = DAGvec[target].next.size();
+
+        for (int j = 0; j < cs_size; j++)
+        {
+          int success = 0;
+          if(tempsize == 0) // edge가 0인 경우 넘어 감
+            break;
+
+          for (int k = 0; k < tempsize; k++){
+            int next = DAGvec[target].next[k];
+            if (visited[next] == false){
+              target = next;
+              change = true;
+              break;
+            }
+          }
+          if(change == true)
+            break;
+        }
+
+        if(change == false){
+          if(target == num_vertices-1)
+            target = 0;
+          for (int i = target + 1; i < num_vertices; i++)
+          {
+            if(visited[i] == true){
+              target = i;
+              change = true;
+              break;
+            }
+          }
+        }
+
+        if(change == false){
+          printf("no way\n");
+          break;
+        }
+      }
+
+      if(change == false){
+        break;
+      }
+    }
+
+    for (int i = 0; i < num_vertices; i++)
+      {
+        printf("%d ", take[i]);
+      }
+      printf("\n");
+  }
+
+  cnt++;
+  */
 }
