9e99690baf61fdcc9cb7f42b5f563ed37f5273af
Jinhee Paeng <jhpaeng306@gmail.com>
2021-06-05 23:39:59 +0900

    added Real time estimation on testAutomate

--- build/main/CMakeFiles/program.dir/DependInfo.cmake
+++ build/main/CMakeFiles/program.dir/DependInfo.cmake
@@ -1,17 +1,19 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jinheepaeng/PrivateGMC/src/backtrack.cc" "/Users/jinheepaeng/PrivateGMC/build/main/CMakeFiles/program.dir/__/src/backtrack.cc.o"
+  "/Users/jinheepaeng/PrivateGMC/src/candidate_set.cc" "/Users/jinheepaeng/PrivateGMC/build/main/CMakeFiles/program.dir/__/src/candidate_set.cc.o"
+  "/Users/jinheepaeng/PrivateGMC/src/graph.cc" "/Users/jinheepaeng/PrivateGMC/build/main/CMakeFiles/program.dir/__/src/graph.cc.o"
+  "/Users/jinheepaeng/PrivateGMC/main/main.cc" "/Users/jinheepaeng/PrivateGMC/build/main/CMakeFiles/program.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/subeen/Developer/SCHOOL/AG/PrivateGMC/src/backtrack.cc" "main/CMakeFiles/program.dir/__/src/backtrack.cc.o" "gcc" "main/CMakeFiles/program.dir/__/src/backtrack.cc.o.d"
-  "/Users/subeen/Developer/SCHOOL/AG/PrivateGMC/src/candidate_set.cc" "main/CMakeFiles/program.dir/__/src/candidate_set.cc.o" "gcc" "main/CMakeFiles/program.dir/__/src/candidate_set.cc.o.d"
-  "/Users/subeen/Developer/SCHOOL/AG/PrivateGMC/src/graph.cc" "main/CMakeFiles/program.dir/__/src/graph.cc.o" "gcc" "main/CMakeFiles/program.dir/__/src/graph.cc.o.d"
-  "/Users/subeen/Developer/SCHOOL/AG/PrivateGMC/main/main.cc" "main/CMakeFiles/program.dir/main.cc.o" "gcc" "main/CMakeFiles/program.dir/main.cc.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
   )
 
 # Targets to which this target links.
--- build/testAutomate.sh
+++ build/testAutomate.sh
@@ -10,7 +10,7 @@ mkdir -p ../OUT/HPRD
 echo "====HPRD_N===="
 for i in 1 3 5 8; do
   echo "* n$i"
-    timeout 60s ./main/program ../data/lcc_hprd.igraph ../query/lcc_hprd_n${i}.igraph ../candidate_set/lcc_hprd_n${i}.cs > ../OUT/HPRD/lcc_hprd_n${i}.txt
+    time timeout 60s ./main/program ../data/lcc_hprd.igraph ../query/lcc_hprd_n${i}.igraph ../candidate_set/lcc_hprd_n${i}.cs > ../OUT/HPRD/lcc_hprd_n${i}.txt
     tail -n1 ../OUT/HPRD/lcc_hprd_n${i}.txt | grep -v 'a'
     wc -l ../OUT/HPRD/lcc_hprd_n${i}.txt | awk '{print $1}'
 done
@@ -18,7 +18,7 @@ done
 echo "====HPRD_S===="
 for i in 1 3 5 8; do
   echo "* s$i"
-    timeout 60s ./main/program ../data/lcc_hprd.igraph ../query/lcc_hprd_s${i}.igraph ../candidate_set/lcc_hprd_s${i}.cs > ../OUT/HPRD/lcc_hprd_s${i}.txt
+    time timeout 60s ./main/program ../data/lcc_hprd.igraph ../query/lcc_hprd_s${i}.igraph ../candidate_set/lcc_hprd_s${i}.cs > ../OUT/HPRD/lcc_hprd_s${i}.txt
     tail -n1 ../OUT/HPRD/lcc_hprd_s${i}.txt | grep -v 'a'
     wc -l ../OUT/HPRD/lcc_hprd_s${i}.txt | awk '{print $1}'
 done
@@ -31,7 +31,7 @@ mkdir ../OUT/HUMAN
 echo "====HUMAN_N===="
 for i in 1 3 5 8; do
   echo "* n$i"
-    timeout 60s ./main/program ../data/lcc_human.igraph ../query/lcc_human_n${i}.igraph ../candidate_set/lcc_human_n${i}.cs > ../OUT/HUMAN/lcc_human_n${i}.txt
+    time timeout 60s ./main/program ../data/lcc_human.igraph ../query/lcc_human_n${i}.igraph ../candidate_set/lcc_human_n${i}.cs > ../OUT/HUMAN/lcc_human_n${i}.txt
     tail -n1 ../OUT/HUMAN/lcc_human_n${i}.txt | grep -v 'a'
     wc -l ../OUT/HUMAN/lcc_human_n${i}.txt | awk '{print $1}'
 done
@@ -39,7 +39,7 @@ done
 echo "====HUMAN_S===="
 for i in 1 3 5 8; do
   echo "* s$i"
-    timeout 60s ./main/program ../data/lcc_human.igraph ../query/lcc_human_s${i}.igraph ../candidate_set/lcc_human_s${i}.cs > ../OUT/HUMAN/lcc_human_s${i}.txt
+    time timeout 60s ./main/program ../data/lcc_human.igraph ../query/lcc_human_s${i}.igraph ../candidate_set/lcc_human_s${i}.cs > ../OUT/HUMAN/lcc_human_s${i}.txt
     tail -n1 ../OUT/HUMAN/lcc_human_s${i}.txt | grep -v 'a'
     wc -l ../OUT/HUMAN/lcc_human_s${i}.txt | awk '{print $1}'
 done
@@ -51,7 +51,7 @@ mkdir ../OUT/YEAST
 echo "====YEAST_N===="
 for i in 1 3 5 8; do
   echo "* n$i"
-    timeout 60s ./main/program ../data/lcc_yeast.igraph ../query/lcc_yeast_n${i}.igraph ../candidate_set/lcc_yeast_n${i}.cs > ../OUT/YEAST/lcc_yeast_n${i}.txt
+    time timeout 60s ./main/program ../data/lcc_yeast.igraph ../query/lcc_yeast_n${i}.igraph ../candidate_set/lcc_yeast_n${i}.cs > ../OUT/YEAST/lcc_yeast_n${i}.txt
     tail -n1 ../OUT/YEAST/lcc_yeast_n${i}.txt | grep -v 'a'
     wc -l ../OUT/YEAST/lcc_yeast_n${i}.txt | awk '{print $1}'
 done
@@ -59,7 +59,7 @@ done
 echo "====YEAST_S===="
 for i in 1 3 5 8; do
     echo "* s$i"
-    timeout 60s ./main/program ../data/lcc_yeast.igraph ../query/lcc_yeast_s${i}.igraph ../candidate_set/lcc_yeast_s${i}.cs > ../OUT/YEAST/lcc_yeast_s${i}.txt
+    time timeout 60s ./main/program ../data/lcc_yeast.igraph ../query/lcc_yeast_s${i}.igraph ../candidate_set/lcc_yeast_s${i}.cs > ../OUT/YEAST/lcc_yeast_s${i}.txt
     tail -n1 ../OUT/YEAST/lcc_yeast_s${i}.txt | grep -v 'a'
     wc -l ../OUT/YEAST/lcc_yeast_s${i}.txt | awk '{print $1}'
 done
